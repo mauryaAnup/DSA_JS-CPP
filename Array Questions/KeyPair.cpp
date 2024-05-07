c7462dac7cf824d5eb0ab4e1d7ee019c87cf7901
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-05-07 23:48:24 +0530

    key pair cpp code

---
+++ Array Questions/KeyPair.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Solution 1 -> Brute Force (T.C -> O(n^2))
+bool findKeyPairBruteForce(int arr[], int n, int x) {
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(arr[i] + arr[j] == x) return true;
+        }
+    }
+    return false;
+}
+
+// Solution 2 -> Sorting & Two Pointer(T.C -> O(nlogn))
+bool findKeyPairOptimized(int arr[], int n, int x) {
+    sort(arr, arr + n);
+    int i = 0;
+    int j = n - 1;
+
+    while(i < j) {
+        int sum = arr[i] + arr[j];
+        if(sum == x) return true;
+        else if(sum < x) i++;
+        else j--;
+    }
+    return false;
+}
+
+int main() {
+    int arr[] = {1, 4, 45, 6, 10, 8};
+    int n = 6;
+    int x = 16;
+
+    // Solution 1
+    findKeyPairBruteForce(arr, n, x);
+
+    // Solution 2
+    findKeyPairOptimized(arr, n, x);
+}
